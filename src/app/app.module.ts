12a782f8a229c7346fc299a3f47d10816a4bceb2
Logan <libinator@gmail.com>
2019-05-07 11:46:42 -0400

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,73 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { VacationComponent } from './components/vacation/vacation.component';
+import { MyvacationsComponent } from './components/vacation/myvacations/myvacations.component';
+import { NewComponent } from './components/vacation/new/new.component';
+import { DetailComponent } from './components/vacation/detail/detail.component';
+import { EventDetailComponent } from './components/vacation/detail/event-detail/event-detail.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { EventTypeComponent } from './components/admin/event-type/event-type.component';
+import { NewEventComponent } from './components/vacation/detail/new-event/new-event.component';
+
+const routes = [
+  { path: '**', children: 
+  [
+    { path: '', component: LandingComponent},
+    { path: 'login', component: LoginComponent},
+    { path: 'signup', component: SignupComponent},
+  ]},
+  { path: 'about', component: AboutComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'admin', children: 
+  [
+    {path: '', component: AdminComponent},
+    {path: 'event-type', component: EventTypeComponent},
+  ]},
+  { path: 'vacation', children: 
+  [
+    { path: '', component: VacationComponent},
+    { path: 'my-vacations', component: MyvacationsComponent},
+    { path: 'new', component: NewComponent},
+    { path: '/:id', children: 
+    [
+      { path: '', component: DetailComponent},
+      { path: 'new-event', component: NewEventComponent},
+      { path: 'detail/:id', component: EventDetailComponent},
+    ]},
+  ]},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    LandingComponent,
+    LoginComponent,
+    SignupComponent,
+    AboutComponent,
+    ContactComponent,
+    VacationComponent,
+    MyvacationsComponent,
+    NewComponent,
+    DetailComponent,
+    EventDetailComponent,
+    AdminComponent,
+    EventTypeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/vacation/myvacations/myvacations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myvacations',
+  templateUrl: './myvacations.component.html',
+  styleUrls: ['./myvacations.component.scss']
+})
+export class MyvacationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
