a52bd81cb5e7164b3e2d6de7e2c3768a0eca7099
MasterTyrr <SamRichards@users.noreply.github.com>
2019-05-09 23:54:32 -0400

    admin portal

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EventTypeService } from 'src/app/services/event-type.service';
+import { EventType } from '../../models/EventType';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _eventTypeServices: EventTypeService) { }
 
   ngOnInit() {
+    this._eventTypeServices.getEventTypeList().subscribe((eventtype: EventType[]) => { this.dataSource =  new MatTableDataSource<EventType>(eventtype)
+    });
   }
-
+  columnNames = [ 'EventTypeID', 'EventTypeName']
+  dataSource: MatTableDataSource<EventType>;
 }
