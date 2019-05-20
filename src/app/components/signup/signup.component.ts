121889c7fec317068fc54c9591e19e93d3f3a146
Logan <libinator@gmail.com>
2019-05-20 13:24:54 -0400

    fixing new form and registration issues

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class SignupComponent implements OnInit {
 
-  private _registerForm: FormGroup;
+  _registerForm: FormGroup;
 
   constructor(private _form: FormBuilder, private _authService: AuthService) { 
     this.createForm();
